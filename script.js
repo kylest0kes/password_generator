f0d3adfeb0b207d07568666a136a26105b8a0aa5
kylest0kes <kylestokes1551@gmail.com>
2020-04-06 20:31:33 -0400

    stated initial variables and made it so the windows only run on click of button

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+document.getElementById("generate").addEventListener("click", function(){
+    var characterLength = prompt("How Many Characters Would You Like Your Password To Be? (Length: 8-128 characters)")
+    var specialCharacters = confirm("Click OK If You Would Like To Include Special Characters (!@#$&*...)")
+    var numberCharacters = confirm("Click OK If You Would Like To Include Numbers")
+    var lowerCharacters = confirm("Click OK If You Would Like Lowercase Characters")
+});
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
